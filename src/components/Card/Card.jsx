91210c7cd398f381fd5631954029b7549d5a60b2
MititelNikolai <mititel.kolya@gmail.com>
2022-08-05 21:38:25 +0300

    Added a crutch for the correct display of images on GitHub pages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './card.css'
 import Point from '../../assets/point.svg'
+import Mount from '../../assets/img/mountFuji.png'
+import Opera from '../../assets/img/operaHouse.png'
+import River from '../../assets/img/river.png'
 const Card = props => {
+	//This function is necessary for the correct display of images on GitHub Pages
+	function selectImg() {
+		switch (props.imgUrl) {
+			case 'mountFuji.png':
+				return Mount
+			case 'operaHouse.png':
+				return Opera
+			case 'river.png':
+				return River
+			default:
+				return 'Image'
+		}
+	}
 	return (
 		<div className='container card__container'>
-			<img src={props.imgUrl} alt={props.title} className='card__img' />
+			<img src={selectImg()} alt={props.title} className='card__img' />
 			<div className='card__info'>
 				<div className='card__title'>
 					<img src={Point} alt='Location point'></img>
